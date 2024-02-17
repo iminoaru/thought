8928b8cf2a3b3ca3e803cdfb38f0e35cb9439c6a
sarthak <sarthakgaud04@gmail.com>
2024-02-17 17:06:23 +0530

    postgres connection sucks

--- server/src/routes/show.ts
+++ server/src/routes/show.ts
@@ -10,7 +10,8 @@ app.get('/' , MW , async (req, res) => {
 
     const textList = await prisma.thoughts.findMany({
         select: {
-            text: true
+            text: true,
+            time: true
         }
     });
     if(textList){
@@ -19,6 +20,25 @@ app.get('/' , MW , async (req, res) => {
     else{
         res.status(400).send({message : "cant fetch textList"})
     }
+})
+
+app.get('/filter' , async (req , res) => {
+    const startDate : string = Array.isArray(req.query.from) ? (req.query.from[0] as string) : (req.query.from as string) || "";
+    const endDate: string = Array.isArray(req.query.to) ? (req.query.to[0] as string) : (req.query.to as string) || ""
+
+    const filteredText = prisma.thoughts.findMany({
+        where: {
+            time : {
+                lte: new Date(endDate).toISOString(), 
+                gte: new Date(startDate).toISOString()
+              }
+        },
+        select: {
+            text : true,
+            time : true
+        }
+        
+    })
 })
 
 
